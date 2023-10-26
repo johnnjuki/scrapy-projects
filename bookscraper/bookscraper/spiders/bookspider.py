195278b3d232df13dd2c3398212da690ed5cb4cd
John Njuki <njukijohn2000@gmail.com>
2023-10-26 15:36:31 +0300

    scrape all book names, price, and book info urls

---
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class BookSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        for book in response.css("article.product_pod"):
+            yield {
+                "name": book.css("h3 a::text").get(),
+                "price": book.css(".product_price .price_color::text").get(),
+                "url": book.css("h3 a").attrib["href"],
+            }
+
+        next_page = response.css("li.next a").attrib["href"]
+        if next_page is not None:
+            if "catalogue/" in next_page:
+                next_page_url = "https://books.toscrape.com/" + next_page
+            else:
+                next_page_url = "https://books.toscrape.com/catalogue/" + next_page
+
+            yield response.follow(next_page_url, callback=self.parse)
